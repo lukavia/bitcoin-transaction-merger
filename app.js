cf4ec5446d9f204eb78051f4d6e5fd941e0ee6f7
Anton Avramov <lukav@lukav.com>
2015-06-29 01:20:40 +0300

    Fix: inputs counting and priority calculation Added: get priority estimate from node

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 require('bitcoin-math');
 
 var bitcoin = require('bitcoin');
-var treshold = 57600000;
+var AllowFreeThreshold = 57600000; // COIN * 144 / 250
 var config = require('./config');
-
-// all config options are optional
+var treshold = AllowFreeThreshold;
 var client = new bitcoin.Client(config.client);
 var HubAddress = config.address;
 
@@ -16,33 +15,45 @@ function mergeTransactions(){
   client.listUnspent(0,function(err, unspend, resHeaders) {
     if (err) return console.log(err);
     unspend.sort(function(a,b){return b.amount.toSatoshi() * b.confirmations - a.amount.toSatoshi() * a.confirmations});
-
-    while (unspend.length > 2){
-      output = {};
-      output[HubAddress] = 0;
-      trx = findTransaction(unspend,output);
-      if (trx) {
-        client.createRawTransaction(trx,output,function(err,rawTransaction){
-          if (err) return console.log(err);
-          client.decodeRawTransaction(rawTransaction,function(err,transaction){
-            if (err) return console.log(err);
-              console.log('Will send txid: '+transaction.txid);
-              console.log('  with total amount of '+transaction.vout[0].value+' to address '+HubAddress);
-              console.log('  rawTransaction: '+rawTransaction);
-            if (config.dryRun){
-              console.log('  dry run nothing is send');
-            } else {
-              client.signRawTransaction(rawTransaction,function(err,signedTransaction){
+    //unspend.sort(function(a,b){return b.amount.toSatoshi() - a.amount.toSatoshi()});
+    //unspend.sort(function(a,b){return b.confirmations - a.confirmations});
+    //console.log(unspend);
+    if(unspend.length < 6) {
+      console.log('Not enought unspend transactions');
+    } else {
+      client.estimatePriority(25,function(err,minPriority){
+        if (err) return console.log(err);
+        if (minPriority > 0){
+          threshold = minPriority;
+        }
+        while (unspend.length > 2){
+          output = {};
+          output[HubAddress] = 0;
+          trx = findTransaction(unspend,output);
+          if (trx) {
+            client.createRawTransaction(trx,output,function(err,rawTransaction){
+              if (err) return console.log(err);
+              client.decodeRawTransaction(rawTransaction,function(err,transaction){
                 if (err) return console.log(err);
-                client.sendRawTransaction(signedTransaction.hex,function(err){
-                  if (err) return console.log(err);
-                  console.log('Send txid: '+transaction.txid);
-                });
+                  console.log('Will send txid: '+transaction.txid);
+                  console.log('  with total amount of '+transaction.vout[0].value+' to address '+HubAddress);
+                  console.log('  rawTransaction: '+rawTransaction);
+                if (config.dryRun){
+                  console.log('  dry run nothing is send');
+                } else {
+                  client.signRawTransaction(rawTransaction,function(err,signedTransaction){
+                    if (err) return console.log(err);
+                    client.sendRawTransaction(signedTransaction.hex,function(err){
+                      if (err) return console.log(err);
+                      console.log('Send txid: '+transaction.txid);
+                    });
+                  });
+                }
               });
-            }
-          });
-        });
-      }
+            });
+          }
+        }
+      });
     }
   });
 }
@@ -51,12 +62,12 @@ function findTransaction(inputs,output){
   var priority = 0;
   var i = 0;
   var res = [];
-  while (i<6 && priority/(((i+1)*34)+10) < treshold && inputs.length){
-    input = inputs.shift();
-    priority = priority + input.amount.toSatoshi() * (input.confirmations+1);
-    output[HubAddress] = output[HubAddress] + input.amount.toSatoshi();
-    res.push({txid:input.txid,vout:input.vout});
+  while (i<6 && priority/(((2)*34)+10) < treshold && inputs.length){// (2) Should be outputs+1
+    priority = priority + inputs[0].amount.toSatoshi() * (inputs[0].confirmations+1);
+    output[HubAddress] = output[HubAddress] + inputs[0].amount.toSatoshi();
+    res.push({txid:inputs[0].txid,vout:inputs[0].vout});
     i++;
+    input = inputs.shift();
   }
   if (i == 6){
     console.log('No transactions with minimum priority found');
