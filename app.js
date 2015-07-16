182f5226b6fb51a99c903e6b961b9f5d14e659cb
Anton Avramov <lukav@lukav.com>
2015-07-17 00:26:22 +0300

    Logic changed to make calculations as the bitcoin-qt does, so the result is the same as if using bitcoin-qt

--- app.js
+++ app.js
@@ -1,23 +1,47 @@
 require('bitcoin-math');
 
 var bitcoin = require('bitcoin');
+var blib = require('bitcoinjs-lib');
 var AllowFreeThreshold = 57600000; // COIN * 144 / 250
 var config = require('./config');
 var threshold = AllowFreeThreshold;
 var client = new bitcoin.Client(config.client);
 var HubAddress = config.address;
+var Addresses = {};
 
 //client.walletPassphrase('',5,function(err,wallet){
 //  if (err) return console.log(err);
 //});
+function getAddresFromScriptPubKey(scriptPubKey){
+  return blib.Address.fromOutputScript(blib.Script.fromHex(scriptPubKey),blib.networks.bitcoin).toString();
+}
+
+function getAddressInfo(tx){
+  var addr = getAddresFromScriptPubKey(tx.scriptPubKey);
+  if (!Addresses[addr] && tx.spendable){
+    client.validateAddress(addr,function(err,data){
+      if (err) return console.log(err);
+      Addresses[addr] = data;
+    });
+  }
+}
+
 function mergeTransactions(){
   console.log((new Date()).toString());
   client.listUnspent(1,function(err, unspend, resHeaders) {
     if (err) return console.log(err);
-    unspend.sort(function(a,b){return b.amount.toSatoshi() * b.confirmations - a.amount.toSatoshi() * a.confirmations});
-    //unspend.sort(function(a,b){return b.amount.toSatoshi() - a.amount.toSatoshi()});
-    //unspend.sort(function(a,b){return b.confirmations - a.confirmations});
-    //console.log(unspend);
+    unspend.sort(function(a,b){
+      // send unspandble to the end
+      if(!a.spendable) return 1;
+      if(!b.spendable) return -1;
+      // this is a hack to get all the addresses info
+      getAddressInfo(a);
+      getAddressInfo(b);
+      // !compressed nInputSize = 29
+      //double dPriority = ((double)out.tx->vout[out.i].nValue  / (nInputSize + 78)) * (out.nDepth+1); // 78 = 2 * 34 + 10
+      return b.amount.toSatoshi() * b.confirmations - a.amount.toSatoshi() * a.confirmations;
+    });
+
     if(unspend.length < 6) {
       console.log('Not enought unspend transactions');
     } else {
@@ -28,7 +52,7 @@ function mergeTransactions(){
         } else {
           threshold = AllowFreeThreshold;
         }
-        while (unspend.length > 2){
+        while (unspend.length > 4){
           var output = {};
           output[HubAddress] = 0;
           var trx = findTransaction(unspend,output);
@@ -79,32 +103,62 @@ function processTransaction(trx,output,callback){
 };
 
 function findTransaction(inputs,output){
-  var priority = 0;
+  var dPriorityInputs = 0.0;
+  var nQuantityUncompressed = 0;
+  var nBytes = 0;
+  var nBytesInputs = 0;
   var i = 0;
   var res = [];
-  while (i<6 && priority/(((2)*34)+10) < threshold && inputs.length){// (2) Should be outputs+1
-    var input = inputs[0];
+
+  function calcTx(){
     if (input.spendable) { // filter watch-only wallet transactions
-      priority = priority + input.amount.toSatoshi() * (input.confirmations+1);
+      var addr = getAddresFromScriptPubKey(input.scriptPubKey);
+      dPriorityInputs = dPriorityInputs + input.amount.toSatoshi() * (input.confirmations+1);
+      if (Addresses[addr] && !Addresses[addr].iscompressed){
+        nQuantityUncompressed++;
+        nBytesInputs += 180;
+      } else {
+        nBytesInputs += 148;
+      }
+      nBytes = nBytesInputs + ((1 * 34) + 10);
+    }
+  }
+  function addTx(){
+    if (input.spendable) { // filter watch-only wallet transactions
       output[HubAddress] = output[HubAddress] + input.amount.toSatoshi();
       res.push({txid:input.txid,vout:input.vout});
       i++;
+      console.log(nBytes,input.amount,nQuantityUncompressed,threshold,dPriorityInputs/(nBytes - nBytesInputs + (nQuantityUncompressed*29)));
     }
-    inputs.shift();
   }
-  if (i == 7){
-    console.log('No transactions with minimum priority found');
+  
+  do {
+    var input = inputs[0];
+    calcTx();
+    addTx();
+    inputs.shift();
+  } while (nBytes<1024 && dPriorityInputs/(nBytes - nBytesInputs + (nQuantityUncompressed*29)) < threshold && inputs.length);
+  console.log('----');
+  if (nBytes > 1024){
+    console.log('Transaction too large');
     return false;
   }
-  while (i<6 && inputs.length){
+  // Add more transactions
+  while (nBytes<1024 && inputs.length){
     input = inputs.pop();
-    if (input.spendable) { // filter watch-only wallet transactions
-      output[HubAddress] = output[HubAddress] + input.amount.toSatoshi();
-      res.push({txid:input.txid,vout:input.vout});
-      i++;
+    calcTx();
+    if (nBytes<1024){
+      addTx();
+    } else if(inputs.length && input.spendable){ //Return last transaction for next pass
+      inputs.unshift(input);
     }
   }
-  if (i < 6){
+  console.log('=====');
+  if(dPriorityInputs/(nBytes - nBytesInputs + (nQuantityUncompressed*29)) < threshold){
+    console.log('Not enought priority');
+    return false;
+  }
+  if (i < 5){
     console.log('Not enought unspend transactions');
     return false;
   }
